e13174181c775bec2d92a5da18598dc24cf82e81
Enes <57043370+Commander-jpg@users.noreply.github.com>
2023-11-02 10:08:48 -0700

    ödev
    ödev

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args) {
+      int a=18;
+        String ad = "Enes";
+        String soyad = "Durgun";
+        String sınıf = "pc teknolojisi";
+        
+            System.out.println(a);
+            System.out.println(ad);
+            System.out.println(soyad);
+            System.out.println(sınıf);
+    }   
